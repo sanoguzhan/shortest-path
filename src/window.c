9b8a401eb4b28b817323d67c65824afbc6685f32
Oguzhan San <sanoguzhan@hotmail.com>
2020-12-25 21:44:50 +0100

    [wip] data structures and terminal prints for frame

--- src/window.c
+++ src/window.c
@@ -1,14 +1,60 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#define N_DIM 20
 
-char cmd[1024];
+typedef void (*func)(unsigned grid[N_DIM][N_DIM]);
+typedef struct Frame
+{
+   unsigned GRID[N_DIM][N_DIM];
+   unsigned index[N_DIM][N_DIM];
+   func show;
 
-int main(int argc, char *argv[]){
-   int grid[100][100];
-   for(size_t i=0; i<100; i++){
-      for(size_t j=0; j<100; j++){
-         //
+} Frame;
+
+Frame init_Frame(){
+   Frame frame;
+   register unsigned loc = 0;
+   for(size_t i=0; i< N_DIM; i++){
+      for(size_t j=0;j< N_DIM; j++){
+         frame.index[i][j]=loc;
+         loc++;
       }
    }
+   return frame;
+}
+
+static unsigned GRID[N_DIM][N_DIM];
+
+void print_grid(int grid[N_DIM][N_DIM]){
+  unsigned item=0;
+  printf("%*s", 30, " ");
+  printf("GRID 20x20", 20);
+  printf("\n");
+
+  for(size_t i=0; i<N_DIM; i++){
+     printf("-");
+      for(size_t j=0; j<N_DIM; j++){
+         
+         printf("----");
+      }
+      puts("");
+      printf("| ");
+      for(size_t j=0; j<N_DIM; j++){
+         item = grid[i][j];
+         printf("%u",item);
+         if(item < 9) printf(" | ");
+         else if (item < 99) printf("| ");
+         else printf("|");
+      }
+     puts(""); 
+   } 
+}
+
+int main(int argc, char* argv[argc +1]){
+   Frame frame = init_Frame();
+   frame.show = print_grid;
+   frame.show(frame.GRID);
+   frame.show(frame.index);
     return 0;
 }
